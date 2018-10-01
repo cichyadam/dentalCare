d40aa064c64db4c4c54e38a675c1229c5ed50321
Adam Cichý <adam@MacBook-Air-de-Adam.local>
2018-10-01 09:38:52 +0200

    mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//after scrolling 200px, navbar will get a background
 $(document).ready(function() {
     $(window).scroll(function() {
       if($(this).scrollTop() > 200) { 
@@ -9,12 +10,26 @@ $(document).ready(function() {
       }
     });
 });
-
+//toggling class of burger menu -> open/close nav
 let hamburger = document.querySelector(".hamburger");
-  hamburger.addEventListener("click", function() {
+  hamburger.addEventListener("click", () =>{
+    if (!hamburger.classList.contains("is-active")){
+        openNav(); 
+    } else {
+        closeNav();
+    }
     hamburger.classList.toggle("is-active");
+   
   });
-
+// close the nav after click on anchor + remove active class from burger menu
+    let link = document.querySelectorAll(".overlay-content a");
+    link.forEach(link => {
+        link.addEventListener("click", () => {
+            hamburger.classList.remove("is-active");
+            closeNav();
+        });
+      });
+   
 function openNav() {
     document.getElementById("mobile-navigation").style.height = "100%";
   }
