ec596a81882903fd6e0200de810ca305eef58848
Adam Cichý <adam@laptop-lbtmfhiv.kea.local>
2018-10-08 12:23:25 +0200

    init

--- js/main.js
+++ js/main.js
@@ -111,12 +111,4 @@ function openNav() {
     document.getElementById("mobile-navigation").style.height = "0%";
   }
 
-  $("#slider").on('slide.bs.carousel', function(evt) {
-
-    var step = $(evt.relatedTarget).index();
-
-    $('#slider_captions .carousel-caption:not(#caption-'+step+')').fadeOut('fast', function() {
-            $('#caption-'+step).fadeIn();
-    });
-
- });
+  
